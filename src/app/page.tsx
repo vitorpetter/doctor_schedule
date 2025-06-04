3d95dbee9f9a9953ffd8bd08810f745ecfd76580
vitorpetter <vitorhmpetter@gmail.com>
2025-06-03 22:58:21 -0300

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 const Home = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <>
+    <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-4xl font-bold mb-4">Welcome to My App</h1>
       <p className="text-lg text-gray-700">This is a simple Next.js app with Tailwind CSS.</p>
+      <Button>Bootcamp</Button>
     </div>
+    </>
   );
 }
 
