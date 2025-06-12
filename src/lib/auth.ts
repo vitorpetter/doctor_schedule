bb2152548319064f474a6da8914bf554cca28a5f
vitorpetter <vitorhmpetter@gmail.com>
2025-06-11 23:05:41 -0300

    feat: add sign in with google

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -10,6 +10,12 @@ export const auth = betterAuth({
         usePlural: true,
         schema, // Your Drizzle schema
     }),
+    socialProviders:{
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        },
+    },
     user:{
         modelName:"usersTable",
     },
